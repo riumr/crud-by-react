1292f1b727d4f550634467c11f25813a40d2c6b5
umuea <108637691+riumr@users.noreply.github.com>
2023-11-06 17:47:20 +0900

    feat : get docs from firestore and push to table

--- src/component/Home.js
+++ src/component/Home.js
@@ -1,34 +1,48 @@
+import { useEffect,useState } from "react";
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
+import { collection,getDocs } from "firebase/firestore"; 
 import configuration from "../firebaseConfig";
-import { collection, getDocs } from "firebase/firestore"; 
 
 const app = initializeApp(configuration);
 const db = getFirestore(app);
 
-function Home(){    
-    async function f() {
-        const querySnapshot = await getDocs(collection(db, "users"));
-        querySnapshot.forEach((doc) => {
-        console.log(`${doc.id} => ${doc.data()}`);
-        });
-    }
-    f()
+function Home(){
+    const [data,setData] = useState([]);
+    useEffect(()=>{
+        const fetchData = async() => {
+            const querySnapShot = await getDocs(collection(db,'indexTable'))
+            const rows = [];
+            querySnapShot.forEach((doc)=>{
+                const rowData = doc.data();
+                rows.push(rowData)
+            })
+        setData(rows)
+        };
+    fetchData()
+    },[])
     return (
         <div>
             <h1>Home</h1>
             <table>
                 <thead>
                 <tr>
-                    <th>this is table</th>
+                    <th>ID</th>
+                    <th>Title</th>
                 </tr>
-                </thead>
+                </thead>  
                 <tbody>
-                {/* DB에서 값 가져올 때 tr, td 추가 */}
-                </tbody>
+                    {data.map((td,index)=>(
+                        <tr key={index}>
+                            <td>{td.id}</td>
+                            <td>{td.title}</td>
+                        </tr>
+                    ))}
+                </tbody>     
             </table>
         </div>
     )
 }
+
 
 export default Home
