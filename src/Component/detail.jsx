88e57c23a20966e484e2fe24e5acf7c20f185fa2
umuea <atrium_grate.0j@icloud.com>
2023-12-01 23:55:37 +0900

    layout : update detail page elements refer to layout

--- src/Component/detail.jsx
+++ src/Component/detail.jsx
@@ -31,8 +31,77 @@ const Detail = () => {
         fetchDoc()
     },[])
     console.log(params)
+
+    const itemPhoto = ({img_path}) => {
+        <div className="">
+            <img src={img_path} alt="item_img" />
+        </div>
+    }
+
     return(
         <div className="container-div">
+            <header className="my-3">
+                <h1 className="text-3xl">Home</h1>
+                <input type="search" className="search-box rounded-[5rem] ms-3 w-[25rem] h-10 p-3"/>
+            </header>
+            <main>
+                <div className="item-photo-buy">
+                    <div className="item-photo">
+                        {/* item photo */}
+                        {itemPhoto()}
+                        {itemPhoto()}
+                        {itemPhoto()}
+                        {itemPhoto()}
+                        {itemPhoto()}
+                    </div>
+                    <div className="item-summary">
+                        {/* item summary */}
+                        <div className="item-price">
+                            {/* price */}
+                        </div>
+                        <div className="item-name">
+                            {/* item name */}
+                        </div>
+                        <div className="item-delivery">
+                            {/* delivery method */}
+                        </div>
+                        <div className="item-amount">
+                            {/* amount of item */}
+                            <div>
+                                <button className="">-</button>
+                                <input type="text" />
+                                <button className="">+</button>
+                            </div>
+                        </div>
+                        <div className="item-buy-save">
+                            <button className="">
+                                {/* buy button */}
+                                buy
+                            </button>
+                            <button className="">
+                                {/* bucket save button */}
+                                save
+                            </button>
+                        </div>
+                    </div>
+                </div>
+                <div className="item-description-review">
+                    <div className="item-description">
+                        {/* item description image */}
+                        <img src="" alt="item_image" />
+                    </div>
+                    <div className="item-rating">
+                        {/* ratings */}
+                        <div></div>
+                        <div></div>
+                    </div>
+                    <div className="item-review">
+                        {/* reviews */}
+                        <div></div>
+                        <div></div>
+                    </div>
+                </div>
+            </main>
             <div className="top-div">
                 {data.map((detailDoc,index)=>(
                     <div key={index}>
@@ -50,6 +119,7 @@ const Detail = () => {
                 <Link to="/">목록</Link>
                 <Link to={`/update/${params}`}>수정</Link>
             </div>
+            <footer></footer>
         </div>
     )
 }
